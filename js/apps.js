6e31078a0554f4e8b76b2f90928a87da7dbba78e
mohammad-qethama <mhmd.a@protonmail.com>
2021-02-16 15:35:02 +0200

    fixed Eslint

--- js/apps.js
+++ js/apps.js
@@ -1,33 +1,34 @@
 'use strict';
-let  catsLove = prompt('do i like cats ? (answer with yes/y or no/n only )');
+let z = 0 ;
+let catsLove = prompt('do i like cats ? (answer with yes/y or no/n only )');
 catsLove = catsLove.toLowerCase();
 
-while (!( catsLove == 'yes' || catsLove == 'y'||  catsLove == 'no' || catsLove == 'n' ))
+while (!( catsLove === 'yes' || catsLove === 'y'|| catsLove === 'no' || catsLove === 'n' ))
 {
   catsLove = prompt('do i like cats ? (answer with yes/y or no/n only )');
   catsLove = catsLove.toLowerCase();
 
 
 }
-console.log(catsLove)
+console.log(catsLove);
 
-if (catsLove == 'yes' || catsLove == 'y' )
+if (catsLove === 'yes' || catsLove === 'y' )
 {
 
   alert('Correct! , Of course i do !') ;
-  
+
 
 }
 else
 {
-  alert('Sadly you answered wrong , i am not a monster :D') ; 
+  alert('Sadly you answered wrong , i am not a monster :D') ;
 
 }
 
 let myStrength = prompt('Am i a patient man ? ');
 myStrength = myStrength.toLowerCase();
 
-while (!( myStrength == 'yes' || myStrength == 'y'||  myStrength == 'no' || myStrength == 'n' ))
+while (!( myStrength === 'yes' || myStrength === 'y'|| myStrength === 'no' || myStrength === 'n' ))
 {
 
   myStrength = prompt('Am i a patient man ? ');
@@ -36,10 +37,10 @@ while (!( myStrength == 'yes' || myStrength == 'y'||  myStrength == 'no' || mySt
 }
 console.log(myStrength);
 
-if ( myStrength== 'yes' || myStrength== 'y' )
+if ( myStrength=== 'yes' || myStrength=== 'y' )
 {
 
-  alert('True , at least i like to thik so') ;
+  alert('True , at least i like to think so') ;
 
 }
 else
@@ -51,7 +52,7 @@ else
 let foodPref= prompt('Do i like sugar ? ');
 foodPref = foodPref.toLowerCase();
 
-while (!( foodPref == 'yes' || foodPref == 'y'||  foodPref == 'no' || foodPref == 'n' ))
+while (!( foodPref === 'yes' || foodPref === 'y'|| foodPref === 'no' || foodPref === 'n' ))
 {
 
   foodPref = prompt('Do i like sugar ? ');
@@ -60,7 +61,7 @@ while (!( foodPref == 'yes' || foodPref == 'y'||  foodPref == 'no' || foodPref =
 }
 console.log(foodPref);
 
-if ( foodPref == 'yes' || foodPref == 'y' )
+if ( foodPref === 'yes' || foodPref === 'y' )
 {
 
   alert('Wrong ,i prefer not having sugar in my food ') ;
@@ -68,38 +69,38 @@ if ( foodPref == 'yes' || foodPref == 'y' )
 }
 else
 {
-  alert('True ,i dont like added sugar at all') ;
-
+  alert('True ,i do not like added sugar at all') ;
+  z++;
 
 }
-let readingPref= prompt('am i intrested in Comparative religion? ');
+let readingPref= prompt('Am i interested in Comparative religion? ');
 readingPref = readingPref.toLowerCase();
 
-while (!( readingPref == 'yes' || readingPref == 'y'||  readingPref == 'no' || readingPref == 'n' ))
+while (!( readingPref === 'yes' || readingPref === 'y'|| readingPref === 'no' || readingPref === 'n' ))
 {
 
-  readingPref = prompt('am i intrested in Comparative religion? ');
+  readingPref = prompt('Am i interested in Comparative religion? ');
   readingPref = readingPref.toLowerCase();
 
 }
 console.log(readingPref);
 
-if ( readingPref == 'yes' || readingPref == 'y' )
+if ( readingPref === 'yes' || readingPref === 'y' )
 {
 
   alert('Yes, i do ') ;
-
+  z++;
 }
 else
 {
-  alert('was a hard question in it ? people tend to hide those intrests now adays but not me.') ;
+  alert('was a hard question in it ? people tend to hide those interests now a days but not me.') ;
 
 
 }
 let langPref= prompt('do you find arabic language superior to other languages ? ');
 langPref = langPref.toLowerCase();
 
-while (!( langPref == 'yes' || langPref == 'y'||  langPref == 'no' || langPref == 'n' ))
+while (!( langPref === 'yes' || langPref === 'y'|| langPref === 'no' || langPref === 'n' ))
 {
 
   langPref = prompt('do you find arabic language superior to other languages ? ');
@@ -108,11 +109,11 @@ while (!( langPref == 'yes' || langPref == 'y'||  langPref == 'no' || langPref =
 }
 console.log(langPref);
 
-if ( langPref== 'yes' || langPref== 'y' )
+if ( langPref=== 'yes' || langPref=== 'y' )
 {
 
   alert('Yes, i do and it is  also the most beautifull one. ') ;
-
+  z++;
 }
 else
 {
@@ -125,5 +126,85 @@ let hisNAME = prompt('What is your name ? ');
 console.log('his Name is '+ hisNAME);
 
 document.getElementById('Welcome').innerHTML = 'Welcome ' +hisNAME +' <i class="fas fa-atom"></i>';
+let x = 4;
+let guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+guessAge = parseInt(guessAge);
+
+while(!guessAge) {
+  alert('Please Enter a number');
+  guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+  guessAge = parseInt(guessAge);
+}
+
+for (x>0 ; x--;) {
+
+  if (guessAge === 26)
+  {
+
+    alert('Right!! :D ');
+    z++;
+    break;
+
+
+  }
+  else if (guessAge < 20 ) {
+    alert('Too low');
+    guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+    guessAge = parseInt(guessAge);
+
+    while(!guessAge) {
+      alert('Please Enter a number');
+      guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+      guessAge = parseInt(guessAge);
+    }
+
+  } else if (guessAge > 35) {
+    alert('Too high');
+
+    guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+    guessAge = parseInt(guessAge);
+
+    while(!guessAge) {
+      alert('Please Enter a number');
+      guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+      guessAge = parseInt(guessAge);
+    }
+
+  } else{
+    alert('Wrong');
+
+    guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+    guessAge = parseInt(guessAge);
+
+    while(!guessAge) {
+      alert('Please Enter a number larger');
+      guessAge = prompt('How old am i ? (you have ' + x + ' iteration to guess ^_^ )');
+      guessAge = parseInt(guessAge);
+    }
+
+  }
+}
+let favFoodFood = prompt('can you guess what food i like?').toLowerCase();
+let favFood = ['burger' , 'shawrma' , 'kabsah', 'mansaf', 'grilles' ];
+for (let i =0 ; i < favFood.length; i++ ) {
+  if (favFoodFood === favFood[i]){
+    alert('Correct!');
+    z++;
+    break;}
+  if (i === 4) {
+    alert('wrong');
+    break;
+
+  }
+}
+alert('Your score is '+z+'/7');
+
+
+
+
+
+
+
+
 
 
